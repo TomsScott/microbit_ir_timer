92686494963b017e750bbe6a9d4760f4f55f62d7
Scott <132684582+TomsScott@users.noreply.github.com>
2023-06-01 16:27:38 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def on_button_pressed_a():
+    global Time
+    Active = 0
+    while Active == 1:
+        Time += 0.1
+        basic.pause(100)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 Time2 = 0
 Lap = 0
 Time = 0
@@ -12,12 +20,6 @@ basic.pause(100)
 basic.clear_screen()
 servos.P2.set_angle(0)
 
-def on_every_interval():
-    global Time
-    Time += 1
-    # Compte le temps en secondes
-loops.every_interval(1000, on_every_interval)
-
 def on_forever():
     global Lap, Time2, Time
     if Lap <= 10:
@@ -26,12 +28,12 @@ def on_forever():
             serial.write_line("Lap " + ("" + str(Lap)) + " finished in ")
             # Montrer le numero du tour avec le temps du tour
             serial.write_number(Time)
-            serial.write_line("")
+            serial.write_line("secondes")
             Time2 = Time + Time2
             serial.write_line("Lap " + ("" + str(Lap)) + " finished at ")
             # Montrer le numero du tour avec le temps totale
             serial.write_number(Time2)
-            serial.write_line("")
+            serial.write_line("secondes")
             Time = 0
             basic.show_number(Lap)
             servos.P2.set_angle(5)
--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 let Time2 = 0
 let Lap = 0
-let Time = 0
 basic.showLeds(`
     . . . . #
     . . . # .
@@ -11,24 +10,19 @@ basic.showLeds(`
 basic.pause(100)
 basic.clearScreen()
 servos.P2.setAngle(0)
-// Compte le temps en secondes
-loops.everyInterval(1000, function () {
-    Time += 1
-})
 basic.forever(function () {
     if (Lap <= 10) {
         if (BitMaker.read_Din_value(GrovePort.P0) == 0) {
             Lap += 1
-            serial.writeLine("Lap " + ("" + Lap) + " finished in ")
-            // Montrer le numero du tour avec le temps du tour
-            serial.writeNumber(Time)
-            serial.writeLine("")
-            Time2 = Time + Time2
             serial.writeLine("Lap " + ("" + Lap) + " finished at ")
             // Montrer le numero du tour avec le temps totale
-            serial.writeNumber(Time2)
-            serial.writeLine("")
-            Time = 0
+            serial.writeNumber(control.millis() / 1000)
+            serial.writeLine(" secondes")
+            serial.writeLine("Lap " + ("" + Lap) + " finished in ")
+            // Montrer le numero du tour avec le temps du tour
+            serial.writeNumber((control.millis() - Time2) / 1000)
+            serial.writeLine(" secondes")
+            Time2 = control.millis()
             basic.showNumber(Lap)
             servos.P2.setAngle(5)
             // Agitation de drapeau
