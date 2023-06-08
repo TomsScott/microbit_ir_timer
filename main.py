5c90c097166a858a26e6063ec94673f31449d28b
Scott <132684582+TomsScott@users.noreply.github.com>
2023-06-08 15:57:12 +0200

    Version 1.0.0: Program fonctionel avec indications

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-"""
-
-Sets the necessary variables in place
-
-"""
+#Startup et preparations du program
 Time2 = 0
-Time = 0
 Lap = 0
 basic.show_leds("""
     . . . . #
@@ -16,27 +11,23 @@ basic.show_leds("""
 basic.pause(100)
 basic.clear_screen()
 servos.P2.set_angle(0)
-"""
-
-Startup logo to demonstrate a successfull startup.
-
-"""
 
 def on_forever():
-    global Lap, Time2, Time
+    global Lap, Time2
     if Lap <= 10:
         if BitMaker.read_Din_value(GrovePort.P0) == 0:
             Lap += 1
-            serial.write_line("Lap " + ("" + str(Lap)) + " finished in ")
-            # Write the lap number and the duration it took to finish said lap.
-            serial.write_number(Time)
-            serial.write_line("secondes")
-            Time2 = Time + Time2
+            serial.write_line("")
             serial.write_line("Lap " + ("" + str(Lap)) + " finished at ")
-            # Write the lap number and the total duration that has passed.
-            serial.write_number(Time2)
-            serial.write_line("secondes")
-            Time = 0
+            # Montrer le numero du tour avec le temps totale en secondes
+            serial.write_number(control.millis() / 1000)
+            serial.write_line(" secondes")
+            serial.write_line("Lap " + ("" + str(Lap)) + " finished in ")
+            # Montrer le numero du tour avec le temps du tour en secondes
+            serial.write_number((control.millis() - Time2) / 1000)
+            serial.write_line(" secondes")
+            Time2 = control.millis()
+            #Variable "checkpoint" pour trouver le temps du prochain tour
             basic.show_number(Lap)
             servos.P2.set_angle(5)
             # Agitation de drapeau
--- main.ts
+++ main.ts
@@ -10,20 +10,24 @@ basic.showLeds(`
 basic.pause(100)
 basic.clearScreen()
 servos.P2.setAngle(0)
+/**
+ * Startup et preparations du program
+ */
 basic.forever(function () {
     if (Lap <= 10) {
         if (BitMaker.read_Din_value(GrovePort.P0) == 0) {
             Lap += 1
             serial.writeLine("")
             serial.writeLine("Lap " + ("" + Lap) + " finished at ")
-            // Montrer le numero du tour avec le temps totale
+            // Montrer le numero du tour avec le temps totale en secondes
             serial.writeNumber(control.millis() / 1000)
             serial.writeLine(" secondes")
             serial.writeLine("Lap " + ("" + Lap) + " finished in ")
-            // Montrer le numero du tour avec le temps du tour
+            // Montrer le numero du tour avec le temps du tour en secondes
             serial.writeNumber((control.millis() - Time2) / 1000)
             serial.writeLine(" secondes")
             Time2 = control.millis()
+            // Variable "checkpoint" pour trouver le temps du prochain tour
             basic.showNumber(Lap)
             servos.P2.setAngle(5)
             // Agitation de drapeau
